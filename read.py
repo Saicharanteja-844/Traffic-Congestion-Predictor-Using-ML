6f3646235d814e5d0dfd487b33d57f45d3079652
Saicharanteja-844 <pudisaicharanteja@gmail.com>
2026-03-24 14:42:05 +0530

    first commit

---
+++ read.py
@@ -0,0 +1,13 @@
+#code to read the data from the METER-LA dataset METR-LA.h5\METR-LA.h5
+# read data and import to excel data sheet
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Load the traffic speed data
+df = pd.read_hdf('METR-LA/METR-LA.h5')
+# Convert the DataFrame to an Excel file
+# df.to_excel('METR-LA.xlsx', index=False)
+
+print(df.head())
